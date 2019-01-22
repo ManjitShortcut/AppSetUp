7afed84ebfacad47d4f28d9a852da406b95ab8e7
manjit sahoo <manjitsahoo20@gmail.com>
2019-01-22 15:22:26 +0100

    Commit source code

--- AppSetUp/AppSetUp/ViewController.swift
+++ AppSetUp/AppSetUp/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getListValues(list: [1,2,3,4,5,6,7], withClosure:{(sum) in
+            print(sum)
+        });
+        
+        let value = colsureWithString("test","guest")
+        print(value);
+        
+       
+        
     }
-
+    func getListValues(list:[Int],withClosure closure:(Int)->Void){
+        for value in list{
+            closure(value);
+        }
+    }
+    
+    func getReturnValueWithClosure( closure:(_ value:String)->String){
+      let returnItems = closure("Manjit");
+        print(returnItems);
+    }
+    
+    let colsureWithString = {(value:String, name:String)->String in
+        return  "Manjit" + value + name
+    }
 
 }
 
